6992db9632f5e8f013845b7e99ce3116e590506d
Мария Задорина <shmv13m@ya.ru>
2022-08-27 00:20:09 +0700

    Сделала описания ошибок константами, убрала try/exept из read_package, переименовала COEFF_CALORIE

--- homework.py
+++ homework.py
@@ -1,6 +1,15 @@
 from typing import Dict, Type, ClassVar
 from dataclasses import dataclass, asdict
 
+CALOTIES_TRAIN_ERROR: str = ('Определите метод get_spent_calories '
+                             'в классе {}.')
+UNKNOWN_TR_TYPE_ERROR: str = (
+    'Ваш трекер пока не умеет работать'
+    'c тренировками подобного типа. '
+    'Напишите нам на address@comp.ru, '
+    'и в одном из следующих обновлений мы его научим.'
+)
+
 
 @dataclass
 class InfoMessage:
@@ -18,7 +27,6 @@ class InfoMessage:
     calories: float
 
     def get_message(self) -> str:
-
         return self.MESSAGE.format(**asdict(self))
 
 
@@ -48,30 +56,33 @@ class Training:
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
-        raise NotImplementedError('Определите метод get_spent_calories '
-                                  'в классе {}.'.format(type(self).__name__))
+        raise NotImplementedError(
+            CALOTIES_TRAIN_ERROR.format(type(self).__name__)
+        )
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
 
-        return InfoMessage(type(self).__name__,
-                           self.duration,
-                           self.get_distance(),
-                           self.get_mean_speed(),
-                           self.get_spent_calories())
+        return InfoMessage(
+            type(self).__name__,
+            self.duration,
+            self.get_distance(),
+            self.get_mean_speed(),
+            self.get_spent_calories()
+        )
 
 
 class Running(Training):
     """Тренировка: бег."""
 
-    COEFF_CALORIE_1: int = 18   # Эти константы названы так в задании
-    COEFF_CALORIE_2: int = 20
+    SPEED_MULTIPLIER: int = 18
+    SPEED_SUBTRACTED: int = 20
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
 
-        return ((self.COEFF_CALORIE_1 * self.get_mean_speed()
-                 - self.COEFF_CALORIE_2)
+        return ((self.SPEED_MULTIPLIER * self.get_mean_speed()
+                 - self.SPEED_SUBTRACTED)
                 * self.weight / self.M_IN_KM
                 * self.duration * self.MIN_IN_HOUR)
 
@@ -79,9 +90,9 @@ class Running(Training):
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
 
-    COEFF_CALORIE_1: float = 0.035
-    COEFF_CALORIE_2: int = 2
-    COEFF_CALORIE_3: float = 0.029
+    WEIGHT_MULTIPLIER: float = 0.035
+    SPEED_MULTIPLIER: int = 2
+    SUM_MULTIPLIER: float = 0.029
 
     def __init__(self,
                  action: int,
@@ -96,10 +107,10 @@ class SportsWalking(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
 
-        return ((self.COEFF_CALORIE_1 * self.weight
+        return ((self.WEIGHT_MULTIPLIER * self.weight
                 + (self.get_mean_speed()
-                   ** self.COEFF_CALORIE_2 // self.height)
-                * self.COEFF_CALORIE_3 * self.weight)
+                   ** self.SPEED_MULTIPLIER // self.height)
+                * self.SUM_MULTIPLIER * self.weight)
                 * self.duration * self.MIN_IN_HOUR)
 
 
@@ -107,8 +118,8 @@ class Swimming(Training):
     """Тренировка: плавание."""
 
     LEN_STEP: float = 1.38
-    COEFF_CALORIE_1: float = 1.1
-    COEFF_CALORIE_2: int = 2
+    SPEED_SUMMAND: float = 1.1
+    WEIGHT_MULTIPLIER: int = 2
 
     def __init__(self,
                  action: int,
@@ -129,8 +140,8 @@ class Swimming(Training):
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
 
-        return ((self.get_mean_speed() + self.COEFF_CALORIE_1)
-                * self.COEFF_CALORIE_2 * self.weight)
+        return ((self.get_mean_speed() + self.SPEED_SUMMAND)
+                * self.WEIGHT_MULTIPLIER * self.weight)
 
 
 TRAINING_TYPES: Dict[str, Type[Training]] = {
@@ -142,19 +153,16 @@ TRAINING_TYPES: Dict[str, Type[Training]] = {
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    try:
+    if workout_type in TRAINING_TYPES:
         return TRAINING_TYPES[workout_type](*data)
-    except KeyError:
-        print('Ваш трекер пока не умеет работать'
-              f'с тренировкой типа {workout_type}. '
-              'Напишите нам на address@comp.ru, '
-              'и в одном из следующих обновлений мы его научим.')
+    else:
+        raise ValueError(UNKNOWN_TR_TYPE_ERROR)
 
 
 def main(training: Training) -> None:
     """Главная функция."""
     info: InfoMessage = training.show_training_info()
-    return print(info.get_message())
+    print(info.get_message())
 
 
 if __name__ == '__main__':
